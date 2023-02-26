69e4c7f8d5077ee20f38a2d5907494a5557a82c4
Anton Sokolkin <97021906+ASreiros@users.noreply.github.com>
2023-02-26 19:10:42 +0200

    game done

---
+++ main.py
@@ -0,0 +1,67 @@
+import art
+import random
+import game_data
+import copy
+
+
+def get_data(lst):
+    sample = random.choice(lst)
+    lst.remove(sample)
+    return [sample, lst]
+
+
+def compare(value_a, value_b):
+    flag = False
+    while not flag:
+        answer = input("Who has more followers? Type 'A' or 'B':").lower()
+        if answer == "a":
+            flag = True
+            if value_a >= value_b:
+                return True
+            else:
+                return False
+        elif answer == "b":
+            flag = True
+            if value_a <= value_b:
+                return True
+            else:
+                return False
+        else:
+            print("That is not an available choice")
+
+
+def print_compare(person_a, person_b):
+    vowels = ["a", "e", "i", "o"]
+    article_a = "a"
+    article_b = "a"
+    if person_a['description'][0] in vowels:
+        article_a = "an"
+    if person_b['description'][0] in vowels:
+        article_b = "an"
+    print(f"Compare A: {person_a['name']}, {article_a} {person_a['description']}, from {person_a['country']}.")
+    print(art.vs)
+    print(f"Against B: {person_b['name']}, {article_b} {person_b['description']}, from {person_b['country']}.")
+    return compare(person_a['follower_count'], person_b['follower_count'])
+
+
+correct = True
+scores = 0
+info = copy.deepcopy(game_data.data)
+
+print(art.logo)
+print("Compare who has more followers on social media")
+print("")
+print("")
+info_a = {}
+[info_b, info] = get_data(info)
+
+while correct:
+    info_a = info_b
+    [info_b, info] = get_data(info)
+    if print_compare(info_a, info_b):
+        scores += 1
+        print(f"Correct! {info_a['name']} is indeed has more followers than {info_b['name']}")
+        print("----------------------------------------------")
+    else:
+        print(f"Sorry, that's wrong. Final score {scores}")
+        correct = False
